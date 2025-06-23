2151f956ac8558c323fb8094bbc0d796e04cdbed
bannaa <holy-bannaa@proton.me>
2025-06-23 23:25:08 +0300

    categorize files

--- flake.nix
+++ flake.nix
@@ -4,10 +4,6 @@
 	inputs = {
 		nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
 
-		stylix = {
-			url = "github:nix-community/stylix/release-25.05";
-			inputs.nixpkgs.follows = "nixpkgs";
-		};
 		home-manager = {
 			url = "github:nix-community/home-manager";
 			inputs.nixpkgs.follows = "nixpkgs";
@@ -15,16 +11,15 @@
 	};
 
 	outputs = { self, nixpkgs, ... }@inputs: {
-		nixosConfigurations.greg = nixpkgs.lib.nixosSystem {
+		nixosConfigurations.greg-butterscotch = nixpkgs.lib.nixosSystem {
 			specialArgs = {inherit inputs;};
 			modules = [
-				./nixos/configuration.nix 
-				inputs.stylix.nixosModules.stylix
+				./profiles/bannaa/nix/configuration.nix
 				inputs.home-manager.nixosModules.home-manager
 				{
 					home-manager.useGlobalPkgs = true;
 					home-manager.useUserPackages = true;
-					home-manager.users.bannaa = import ./home-manager/home.nix;
+					home-manager.users.bannaa = import ./profiles/bannaa/home-manager/home.nix;
 					home-manager.extraSpecialArgs = {inherit inputs;};
 				}
 			];
