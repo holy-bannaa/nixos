17f4c3957e770c2c05fbfde3955c2f0e451ea320
bannaa <holy-bannaa@proton.me>
2025-07-15 18:54:55 +0300

    fish works now :D

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -7,16 +7,12 @@
 		programs.fish = {
 		enable = true;
 		generateCompletions = true;
-		shellAliases = {
-			gs = "git status";
-			ga = "git add";
-			gc = "git commit -m";
-			n = "nivm";
-		};
 		shellAbbrs = {
 			gs = "git status";
 			ga = "git add";
 			gc = "git commit -m";
+			n = "nvim";
+			upnix = "sudo nixos-rebuild switch --flake ~/.config/nixos#greg-butterscotch";
 		};
 	};
 
