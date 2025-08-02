54c9a04c7fbeadecd2b7ce44d5312e82d637da42
bannaa <holy-bannaa@proton.me>
2025-08-03 01:01:27 +0300

    I am a FUCKIGN jenius!!

--- home.nix
+++ home.nix
@@ -12,16 +12,21 @@
 		programs.home-manager.enable = true;
 
 		# symlink .dotfiles
-		home.file = {
-		".config/dconf".source = ./home/dotfiles/dconf;
-		".config/git".source = ./home/dotfiles/git;
-		".config/Vencord".source = ./home/dotfiles/Vencord;
-		".config/gtk-3.0".source = ./home/dotfiles/gtk-3.0;
-		".config/gtk-4.0".source = ./home/dotfiles/gtk-4.0;
-		".config/kitty".source = ./home/dotfiles/kitty;
-		".config/qt6ct".source = ./home/dotfiles/qt6ct;
-		".config/rofi".source = ./home/dotfiles/rofi;
-		".config/xsettingsd".source = ./home/dotfiles/xsettingsd;
-		".config/yazi".source = ./home/dotfiles/yazi;
-		};
+#	home.file = {
+#	".config/dconf" = {source = ./home/dotfiles/dconf; recursive = true;};
+#	".config/git" = {source = ./home/dotfiles/git; recursive = true;};
+#	".config/Vencord" = {source = ./home/dotfiles/Vencord; recursive = true;};
+#	".config/gtk-3.0" = {source = ./home/dotfiles/gtk-3.0; recursive = true;};
+#	".config/gtk-4.0" = {source = ./home/dotfiles/gtk-4.0; recursive = true;};
+#	".config/kitty" = {source = ./home/dotfiles/kitty; recursive = true;};
+#	".config/qt6ct" = {source = ./home/dotfiles/qt6ct; recursive = true;};
+#	".config/rofi" = {source = ./home/dotfiles/rofi; recursive = true;};
+#	".config/xsettingsd" = {source =  ./home/dotfiles/xsettingsd; recursive = true;};
+#	".config/yazi" = {source = ./home/dotfiles/yazi; recursive = true;};
+#};
+
+		home.activation.sym = lib.hm.dag.entryAfter ["writeBoundary"] ''
+			ln -sf ~/.config/nixos/home/dotfiles/* .			
+			'';
 }
+
