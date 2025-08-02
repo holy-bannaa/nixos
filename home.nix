e92c5c0898d04c81e7bcaf7aa2f6905578a3c6ff
bannaa <holy-bannaa@proton.me>
2025-08-02 23:22:45 +0300

    fix maybe4

--- home.nix
+++ home.nix
@@ -12,5 +12,16 @@
 		programs.home-manager.enable = true;
 
 		# symlink .dotfiles
-		home.file.".config".source = ./home/dotfiles;
+		home.file = {
+		".config/dconf".source = ./home/dotfiles/dconf;
+		".config/git".source = ./home/dotflies/git;
+		".config/gtk-3.0".source = ./home/dotflies/gtk-3.0;
+		".config/gtk-4.0".source = ./home/dotflies/gtk-4.0;
+		".config/kitty".source = ./home/dotflies/kitty;
+		".config/qt6ct".source = ./home/dotflies/qt6ct;
+		".config/rofi".source = ./home/dotflies/rofi;
+		".config/Vencord".source = ./home/dotflies/Vencord;
+		".config/xsettingsd".source = ./home/dotflies/xsettingsd;
+		".config/yazi".source = ./home/dotflies/yazi;
+		};
 }
