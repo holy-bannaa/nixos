db4bd23bf43cc16560de704f46d468aa88a9ab7f
bannaa <holy-bannaa@proton.me>
2025-08-02 23:29:03 +0300

    final fix maybe

--- home.nix
+++ home.nix
@@ -14,14 +14,14 @@
 		# symlink .dotfiles
 		home.file = {
 		".config/dconf".source = ./home/dotfiles/dconf;
-		".config/git".source = ./home/dotflies/git;
-		".config/gtk-3.0".source = ./home/dotflies/gtk-3.0;
-		".config/gtk-4.0".source = ./home/dotflies/gtk-4.0;
-		".config/kitty".source = ./home/dotflies/kitty;
-		".config/qt6ct".source = ./home/dotflies/qt6ct;
-		".config/rofi".source = ./home/dotflies/rofi;
-		".config/Vencord".source = ./home/dotflies/Vencord;
-		".config/xsettingsd".source = ./home/dotflies/xsettingsd;
-		".config/yazi".source = ./home/dotflies/yazi;
+		".config/git".source = ./home/dotfiles/git;
+		".config/Vencord".source = ./home/dotfiles/Vencord;
+		".config/gtk-3.0".source = ./home/dotfiles/gtk-3.0;
+		".config/gtk-4.0".source = ./home/dotfiles/gtk-4.0;
+		".config/kitty".source = ./home/dotfiles/kitty;
+		".config/qt6ct".source = ./home/dotfiles/qt6ct;
+		".config/rofi".source = ./home/dotfiles/rofi;
+		".config/xsettingsd".source = ./home/dotfiles/xsettingsd;
+		".config/yazi".source = ./home/dotfiles/yazi;
 		};
 }
