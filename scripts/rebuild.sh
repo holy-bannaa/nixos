5353aaa87060728552fb8d943835e446cdd43823
bannaa <holy-bannaa@proton.me>
2025-08-11 17:30:50 +0300

    118 current  2025-08-11 17:27:30  25.05.20250726.13e8d35  6.12.40                          *

--- scripts/rebuild.sh
+++ scripts/rebuild.sh
@@ -2,7 +2,7 @@
 git diff -U0 *.nix
 echo "NixOS is Rebuilding..."
 sudo nixos-rebuild switch --flake ~/.config/nixos#greg-butterscotch &>~/logs/nixos-switch.log 
-cat ~/logs/nixos-switch.log | grep --color error
+cat ~/logs/nixos-switch.log
 git add ~/.config/nixos
 gen=$(nixos-rebuild list-generations | grep current)
 git commit -m "$gen"
